491479dc0d2b07f2b077ed39bef5f19dc2796256
wanghongfei01 <wanghongfei01@baidu.com>
2019-02-25 11:47:55 +0800

    init project

---
+++ spring-boot-vertx-autoconfigure/src/main/java/cn/fh/springboot/starter/vertx/VertxAutoConfiguration.java
@@ -0,0 +1,10 @@
+package cn.fh.springboot.starter.vertx;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by wanghongfei on 2019-02-25.
+ */
+@Configuration
+public class VertxAutoConfiguration {
+}
